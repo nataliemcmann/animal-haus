1697a68d59f036bc9f47e2d7bfebc5959fee2b7b
Natalie McMann <nmcmann110@gmail.com>
2023-02-06 16:02:41 -0600

    grid/card mui layout established for pet profile

--- src/components/Pet Components/PetProfile/PetProfile.jsx
+++ src/components/Pet Components/PetProfile/PetProfile.jsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 //components
 import DeleteButton from '../../Buttons/DeleteButton/DeleteButton';
+//mui components
+import { Box, Card, CardContent } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function PetProfile () {
     //declare dispatch
@@ -22,10 +25,71 @@ function PetProfile () {
     }, [])
 
     return (
-        <div>
-            <DeleteButton className="petDelete" pet={pets.singlePetReducer}/>
-            <h2>Here is {pets.singlePetReducer && pets.singlePetReducer.name}</h2>
-        </div> 
+        <>
+            <Box sx={{
+                width: 318,
+                height: 480,
+                m: 1}}
+                >
+                <Grid 
+                container spacing={2}
+                m={2}
+                >
+                            <DeleteButton 
+                            className="petDelete" 
+                            pet={pets.singlePetReducer}
+                            />
+                </Grid>
+                <Grid
+                container spacing={4}
+                marginTop={2}
+                marginBottom={2}
+                justifyContent="space-evenly"
+                alignItems="center"
+                >
+                    <Card 
+                        sx={{
+                            flexGrow: 1,
+                            marginLeft: 2,
+                            marginRight: 2
+                        }}
+                    >
+                        <CardContent>
+                        <h3>{pets.singlePetReducer.name}</h3>
+                        </CardContent>
+                    </Card>
+                    <Card 
+                        sx={{
+                            flexGrow: 1,
+                            marginRight: 4
+                        }}
+                    >
+                        <CardContent>
+                        <h3>Age: {pets.singlePetReducer.age}</h3>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid 
+                container spacing={2}
+                marginTop={2}
+                marginBottom={2}
+                >
+                    <Card sx={{
+                            flexGrow: 1,
+                            marginLeft: 1,
+                            marginRight: 3
+                            }}>
+                        <CardContent>
+                        <p>
+                            {pets.singlePetReducer.cups_per_feeding} cup of {pets.singlePetReducer.food_desc} 
+                        </p>
+                        </CardContent>
+                    </Card>
+
+                </Grid>
+            
+            </Box>
+        </> 
     )
 }
 
