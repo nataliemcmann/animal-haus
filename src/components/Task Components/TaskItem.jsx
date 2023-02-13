aee5c2fe5d940f5120fa724d551f0c6ff016e913
Natalie McMann <nmcmann110@gmail.com>
2023-02-13 15:49:05 -0600

    replaced UnclaimTaskButton with ClaimChip component on taskItem for task summary page

--- src/components/Task Components/TaskItem.jsx
+++ src/components/Task Components/TaskItem.jsx
@@ -12,33 +12,35 @@ function TaskItem({ task }) {
     const user = useSelector((store) => store.user);
 
     return(
-        <li> 
-            <Grid
-                marginLeft={1}
-                marginRight={1}
-            >
-                <Grid                                 
-                    container spacing={2}
-                    marginTop={1}
-                    paddingTop={1}
-                    paddingRight={1}
-                    justifyContent="flex-end"
+        <>
+            <li> 
+                <Grid
+                    marginLeft={1}
+                    marginRight={1}
                 >
-                    <CompleteIcon task={task}/>
-                </Grid>
+                    <Grid                                 
+                        container spacing={2}
+                        marginTop={1}
+                        paddingTop={1}
+                        paddingRight={1}
+                        justifyContent="flex-end"
+                    >
+                        <CompleteIcon task={task}/>
+                    </Grid>
                 <Typography>Pet: {task.name}</Typography>
                 <Typography>{task.frequency} Task: {task.taskDesc}</Typography> 
-                <Grid
-                    container spacing={2}
-                    marginTop={1}
-                    justifyContent="space-evenly"
-                >
-                    <UnclaimTaskButton task={task}/>
-                    <DeleteButton className="taskDelete" task = {task}/>
-                    <EditButton className="taskEdit" task = {task}/>
+                    <Grid
+                        container spacing={2}
+                        marginTop={1}
+                        justifyContent="space-evenly"
+                    >
+                        <ClaimStatusChip id={user.id} task={task}/>
+                        <DeleteButton className="taskDelete" task = {task}/>
+                        <EditButton className="taskEdit" task = {task}/>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </li>
+            </li>
+        </>
     )
 }
 
--- src/components/Task Components/UserTaskItem.jsx
+++ src/components/Task Components/UserTaskItem.jsx
@@ -11,35 +11,35 @@ import { Grid, Typography } from '@mui/material';
 
 function UserTaskItem({ task }) {
 
-        return(
-                    <li> 
-                        <Grid
-                            marginLeft={1}
-                            marginRight={1}
-                        >
-                            <Grid                                 
-                                container spacing={2}
-                                marginTop={1}
-                                paddingTop={1}
-                                paddingRight={1}
-                                justifyContent="flex-end"
-                            >
-                                <CompleteIcon task={task}/>
-                            </Grid>
-                            <Typography>Pet: {task.name}</Typography>
-                            <Typography>{task.frequency} Task: {task.taskDesc}</Typography> 
-                            <Grid
-                                container spacing={2}
-                                marginTop={1}
-                                justifyContent="space-evenly"
-                            >
-                                <UnclaimTaskButton task={task}/>
-                                <DeleteButton className="taskDelete" task = {task}/>
-                                <EditButton className="taskEdit" task = {task}/>
-                            </Grid>
-                        </Grid>
-                    </li>
-        )
+return(
+        <li> 
+            <Grid
+                marginLeft={1}
+                marginRight={1}
+            >
+                <Grid                                 
+                    container spacing={2}
+                    marginTop={1}
+                    paddingTop={1}
+                    paddingRight={1}
+                    justifyContent="flex-end"
+                >
+                    <CompleteIcon task={task}/>
+                </Grid>
+                <Typography>Pet: {task.name}</Typography>
+                <Typography>{task.frequency} Task: {task.taskDesc}</Typography> 
+                <Grid
+                    container spacing={2}
+                    marginTop={1}
+                    justifyContent="space-evenly"
+                >
+                    <UnclaimTaskButton task={task}/>
+                    <DeleteButton className="taskDelete" task = {task}/>
+                    <EditButton className="taskEdit" task = {task}/>
+                </Grid>
+            </Grid>
+        </li>
+    )
 }
 
 export default UserTaskItem;
