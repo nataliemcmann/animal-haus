dfc09877d3e49b8e088a1e528082e18249baec59
Natalie McMann <nmcmann110@gmail.com>
2023-02-13 15:45:06 -0600

    now whitespace from user task list item is copied to task list item from summary page

--- src/components/Task Components/TaskItem.jsx
+++ src/components/Task Components/TaskItem.jsx
@@ -6,20 +6,39 @@ import EditButton from '../Buttons/EditButton';
 import CompleteIcon from '../Buttons/CompleteIcon';
 import ClaimStatusChip from './ClaimStatusChip';
 //mui components
+import { Grid, Typography } from '@mui/material';
 
 function TaskItem({ task }) {
     const user = useSelector((store) => store.user);
 
     return(
-        <>
-            <li> 
-                <CompleteIcon task={task}/>
-                {task.name}: {task.taskDesc} {task.frequency} 
-                <ClaimStatusChip id={user.id} task={task}/>
-                <DeleteButton className="taskDelete" task = {task}/>
-                <EditButton className="taskEdit" task = {task}/>
-            </li>
-        </>
+        <li> 
+            <Grid
+                marginLeft={1}
+                marginRight={1}
+            >
+                <Grid                                 
+                    container spacing={2}
+                    marginTop={1}
+                    paddingTop={1}
+                    paddingRight={1}
+                    justifyContent="flex-end"
+                >
+                    <CompleteIcon task={task}/>
+                </Grid>
+                <Typography>Pet: {task.name}</Typography>
+                <Typography>{task.frequency} Task: {task.taskDesc}</Typography> 
+                <Grid
+                    container spacing={2}
+                    marginTop={1}
+                    justifyContent="space-evenly"
+                >
+                    <UnclaimTaskButton task={task}/>
+                    <DeleteButton className="taskDelete" task = {task}/>
+                    <EditButton className="taskEdit" task = {task}/>
+                </Grid>
+            </Grid>
+        </li>
     )
 }
 
