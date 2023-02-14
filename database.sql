633665d98a058670ee5965a168284075f75f5ec2
Natalie McMann <nmcmann110@gmail.com>
2023-02-14 10:35:05 -0600

    updated database sql to include ON DELETE CASCADE in tables with foreign keys

--- database.sql
+++ database.sql
@@ -3,22 +3,23 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- create user table
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
 
---pets create table
+-- create pets table
 CREATE TABLE "pets" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (80) NOT NULL,
     "age" VARCHAR (20),
-    "food_desc" VARCHAR (1000),
-    "cups_per_feeding" DECIMAL (3, 2),
-    "exercise_desc" VARCHAR (1000),
-    "exercise_min" VARCHAR (20),
-    "user_id" INT REFERENCES "user" NOT NULL
+    "foodDesc" VARCHAR (1000),
+    "cupsPerFeed" DECIMAL (3, 2),
+    "exerciseDesc" VARCHAR (1000),
+    "exerciseMin" VARCHAR (20),
+    "userId" INT REFERENCES "user" NOT NULL ON DELETE CASCADE
 );
 
 -- create tasks table
@@ -26,13 +27,19 @@ CREATE TABLE "tasks" (
 	"id" SERIAL PRIMARY KEY,
 	"taskDesc" VARCHAR (1000) NOT NULL,
 	"frequency" VARCHAR (250),
-	"petID" INT REFERENCES "pets" NOT NULL
+	"petID" INT REFERENCES "pets" NOT NULL ON DELETE CASCADE
 );
 
 -- create task_complete table
 CREATE TABLE "task_complete" (
 	"id" SERIAL PRIMARY KEY,
-	"status" BOOLEAN,
 	"timeCompleted" DATE NOT NULL DEFAULT CURRENT_DATE, 
-	"taskID" INT REFERENCES "tasks" NOT NULL
+	"taskID" INT REFERENCES "tasks" NOT NULL ON DELETE CASCADE
+);
+
+-- create tasks_user join
+CREATE TABLE "tasks_user" (
+	"id" SERIAL PRIMARY KEY,
+	"taskID" INT REFERENCES "tasks" NOT NULL ON DELETE CASCADE
+	"userID" INT REFERENCES "user" NOT NULL ON DELETE CASCADE
 );
