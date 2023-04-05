e5c1aa9e1b58402278e2c972354c70c0029d1787
Natalie McMann <nmcmann110@gmail.com>
2023-04-05 13:32:42 -0500

    added post route to create household

--- server/routes/households.router.js
+++ server/routes/households.router.js
@@ -1,8 +1,33 @@
+const { sliderClasses } = require('@mui/material');
 const express = require('express');
-const {
-    rejectUnauthenticated,
-  } = require('../modules/authentication-middleware');
-  const encryptLib = require('../modules/encryption');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
+const householdStrategy = require('../strategies/households.strategy');
 
 const router = express.Router();
+
+//handles post of new household data
+router.post('/register', (req, res, next) => {
+    const adminId = req.user.id;
+    const householdName = req.body.householdName;
+    const householdCode = encryptLib.encryptPassword(req.body.householdCode);
+
+    const sqlQuery = `
+    INSERT INTO "households" 
+        (adminId, householdName, householdCode)
+    VALUES 
+        ($1, $2, $3) RETURNING "id";
+    `
+    pool.query(sqlQuery, [adminId, householdName, householdCode])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+        console.log('Household registration failed: ', err);
+        res.sendStatus(500);
+    });
+});
+
+
+//handles post of new household_user relation
+
+module.exports = router;
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -6,6 +6,7 @@ const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
 
+
 const router = express.Router();
 
 // Handles Ajax request for user information if user is authenticated
