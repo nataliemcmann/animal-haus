a262c3f910f9bbac1374a190abfe25868226d622
Natalie McMann <nmcmann110@gmail.com>
2023-04-14 14:57:04 -0500

    updated household-login-middleware that will be used to create user-household relations

--- server/modules/household-login-middleware.js
+++ server/modules/household-login-middleware.js
@@ -1,9 +1,9 @@
 const checkHouseholdRelation = (household, req, res, next) => {
     console.log(household)
-    if (req.user.householdId === undefined) {
-        next();
-    } else if (req.user.householdId === household.id){
+    if (req.user.householdId === household.id){
         res.sendStatus(403)
+    } else {
+        next();
     }
 }
 
