567cb82b06d1a09c8a77586719953ff4c2f10a4a
Natalie McMann <nmcmann110@gmail.com>
2023-04-20 11:27:40 -0600

    moved daily task completion check to a helper function

--- server/modules/addTaskStatus.js
+++ server/modules/addTaskStatus.js
@@ -10,18 +10,30 @@ function addTaskStatus(taskArray){
             //get sqlDate in format "YEAR-MM-DD"
             sqlDate = reformatDate((task.timeCompleted));
         }
-        //conditional to compare current date to sqlDate if not null
-        if (matchDates(currentDate, sqlDate)) {
-            //if matchDates returns true
-            // then currentDate and sqlDate don't match
-            task.status = 'false'; //add status property of false
-        } else {
-            //if false, the dates match
-            task.status = 'true'; //add status property of true
+        if (task.frequency === 'Daily') {
+            setStatusIfDaily(task, currentDate, sqlDate)
         }
+        //  else if (task.frequnecy === 'Weekly') {
+
+        // }
     }
     return taskArray;
 }
+
+//helper function for daily frequency
+function setStatusIfDaily(task, currentDate, sqlDate) {
+    //conditional to compare current date to sqlDate if not null
+    if (matchDates(currentDate, sqlDate)) {
+        //if matchDates returns true
+        // then currentDate and sqlDate don't match
+        task.status = 'false'; //add status property of false
+    } else {
+        //if false, the dates match
+        task.status = 'true'; //add status property of true
+    }
+}
+
+//helper function for weekly frequency
 
 //helper function to change sqlDate
 function reformatDate(date) {
